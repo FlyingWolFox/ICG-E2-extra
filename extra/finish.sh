b0b923e9f1979ba49829f0218eee69e092a854cf
flyingwolfox <49326973+FlyingWolFox@users.noreply.github.com>
2021-05-06 14:26:47 -0300

    update other files to extra

--- extra/finish.sh
+++ extra/finish.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-sed -i "s/import \* as THREE from 'three';/import * as THREE from 'https:\/\/cdn.skypack.dev\/three@0.128.0';/" js/renderer.js
-sed -i "s/import \* as THREE from 'three';/import * as THREE from 'https:\/\/cdn.skypack.dev\/three@0.128.0';/" js/transformations.js
-sed -i "s/import \* as THREE from 'three';/import * as THREE from 'https:\/\/cdn.skypack.dev\/three@0.128.0';/" js/camera.js
-sed -i "s/import { Canvas } from '\.\/canvas';/import { Canvas } from '.\/canvas.js';/" js/rendere.js
-sed -i "s/import { Canvas } from '\.\/canvas';/import { Canvas } from '.\/canvas.js';/" js/line.js
-sed -i "s/import { Camera } from '\.\/camera';/import { Camera } from '.\/camera.js';/" js/renderer.js
-sed -i "s/import { Transformation } from '\.\/transformations';/import { Transformation } from '.\/transformations.js';/" js/renderer.js
-sed -i "s/import \* as Line from '\.\/line';/import * as Line from '.\/line.js';/" js/index.js
+sed -i "s/'three'/'https:\/\/cdn.skypack.dev\/three@0.128.0'/" js/renderer.js
+sed -i "s/'three'/'https:\/\/cdn.skypack.dev\/three@0.128.0'/" js/transformations.js
+sed -i "s/'three'/'https:\/\/cdn.skypack.dev\/three@0.128.0'/" js/camera.js
+sed -i "s/'\.\/canvas'/'.\/canvas.js'/" js/renderer.js
+sed -i "s/'\.\/canvas'/'.\/canvas.js'/" js/line.js
+sed -i "s/'\.\/camera'/'.\/camera.js'/" js/renderer.js
+sed -i "s/'\.\/camera'/'.\/camera.js'/" js/index.js
+sed -i "s/'\.\/transformations'/'.\/transformations.js'/" js/camera.js
+sed -i "s/'\.\/transformations'/'.\/transformations.js'/" js/renderer.js
+sed -i "s/'\.\/transformations'/'.\/transformations.js'/" js/index.js
+sed -i "s/'\.\/line'/'.\/line.js'/" js/renderer.js
+sed -i "s/'\.\/renderer'/'.\/renderer.js'/" js/index.js
 echo "Done!"
