a489850b2a25e5f1116a03bb43a55caab2eb2b62
flyingwolfox <49326973+FlyingWolFox@users.noreply.github.com>
2021-05-06 14:25:53 -0300

    changed member name

--- extra/src/camera.ts
+++ extra/src/camera.ts
@@ -6,19 +6,19 @@ type Coord = [number, number, number]
 export interface Camera {
     pos: Coord,
     up: Coord,
-    direction: Coord,
+    look_at: Coord,
     focal_point: number
 }
 export class Camera {
     pos: Coord
     up: Coord
-    direction: Coord
+    look_at: Coord
     focal_point: number
 
     constructor(pos: Coord, up: Coord = [0, 1, 0], direction: Coord = [0, 0, 0], focal_point = 1) {
         this.pos = pos
         this.up = up
-        this.direction = direction
+        this.look_at = direction
         this.focal_point = focal_point
     }
 }
@@ -26,7 +26,7 @@ export class Camera {
 function clone_cam(cam: Camera): Camera {
     return {pos: cam.pos,
             up: cam.up,
-            direction: cam.direction,
+            look_at: cam.look_at,
             focal_point: cam.focal_point}
 }
 
@@ -34,7 +34,7 @@ function rotate_horizontal(cam: Camera, degrees: 0) {
     let util_matrix = new THREE.Matrix4()
     util_matrix = Transform.rotate_y(util_matrix, degrees)
     let new_cam = clone_cam(cam)
-    new_cam.direction = new THREE.Vector3().fromArray(cam.direction)
+    new_cam.look_at = new THREE.Vector3().fromArray(cam.look_at)
                         .applyMatrix4(util_matrix).toArray()
     return new_cam
 }
@@ -43,7 +43,7 @@ function rotate_vertical(cam: Camera, degrees: 0) {
     let util_matrix = new THREE.Matrix4()
     util_matrix = Transform.rotate_x(util_matrix, degrees)
     let new_cam = clone_cam(cam)
-    new_cam.direction = new THREE.Vector3().fromArray(cam.direction)
+    new_cam.look_at = new THREE.Vector3().fromArray(cam.look_at)
                         .applyMatrix4(util_matrix).toArray()
     return new_cam
 }
@@ -59,9 +59,9 @@ function rotate_clockwise(cam: Camera, degrees: 0) {
 
 function translate(cam: Camera, by: [number, number, number]) {
     let new_cam = clone_cam(cam)
-    new_cam.direction[0] += by[0]
-    new_cam.direction[1] += by[1]
-    new_cam.direction[2] += by[2]
+    new_cam.look_at[0] += by[0]
+    new_cam.look_at[1] += by[1]
+    new_cam.look_at[2] += by[2]
     return new_cam
 }
 
@@ -69,7 +69,7 @@ function look_straight(cam: Camera) {
     let util_matrix = new THREE.Matrix4()
     util_matrix = Transform.rotate_x(util_matrix, 90)
     let new_cam = clone_cam(cam)
-    new_cam.direction = new THREE.Vector3().fromArray(cam.up)
+    new_cam.look_at = new THREE.Vector3().fromArray(cam.up)
                         .applyMatrix4(util_matrix).toArray()
     return new_cam
 }
