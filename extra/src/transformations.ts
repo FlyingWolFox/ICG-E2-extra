76e5e7808aa4b016313e28feb716b1b9968c8b31
flyingwolfox <49326973+FlyingWolFox@users.noreply.github.com>
2021-05-06 14:25:25 -0300

    fixed non initializantion

--- extra/src/transformations.ts
+++ extra/src/transformations.ts
@@ -8,7 +8,7 @@ export import Transformation = THREE.Matrix4
 
 // returns [cos(), sin(), -sin(), cos()]
 function create_rotate_submatrix(degrees: number) {
-    let subm: number[]
+    let subm = [0, 0, 0, 0]
     degrees = degrees*(Math.PI/180)
     subm[0] = Math.cos(degrees)
     subm[1] = Math.sin(degrees)
