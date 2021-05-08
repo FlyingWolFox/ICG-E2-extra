21836c06cba88d0dd979d031898cbb1644e8c34f
flyingwolfox <49326973+FlyingWolFox@users.noreply.github.com>
2021-05-08 19:25:40 -0300

    extras is now the main repo

--- finish.sh
+++ finish.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-sed -i "s/import \* as THREE from 'three';/import * as THREE from 'https:\/\/cdn.skypack.dev\/three@0.128.0';/" js/index.js
-sed -i "s/import { Canvas } from '\.\/canvas';/import { Canvas } from '.\/canvas.js';/" js/index.js
-sed -i "s/import { Canvas } from '\.\/canvas';/import { Canvas } from '.\/canvas.js';/" js/line.js
-sed -i "s/import \* as Line from '\.\/line';/import * as Line from '.\/line.js';/" js/index.js
+sed -i "s/'three'/'https:\/\/cdn.skypack.dev\/three@0.128.0'/" js/renderer.js
+sed -i "s/'three'/'https:\/\/cdn.skypack.dev\/three@0.128.0'/" js/transformations.js
+sed -i "s/'three'/'https:\/\/cdn.skypack.dev\/three@0.128.0'/" js/camera.js
+sed -i "s/'\.\/canvas'/'.\/canvas.js'/" js/renderer.js
+sed -i "s/'\.\/canvas'/'.\/canvas.js'/" js/line.js
+sed -i "s/'\.\/camera'/'.\/camera.js'/" js/renderer.js
+sed -i "s/'\.\/camera'/'.\/camera.js'/" js/index.js
+sed -i "s/'\.\/transformations'/'.\/transformations.js'/" js/camera.js
+sed -i "s/'\.\/transformations'/'.\/transformations.js'/" js/renderer.js
+sed -i "s/'\.\/transformations'/'.\/transformations.js'/" js/index.js
+sed -i "s/'\.\/line'/'.\/line.js'/" js/renderer.js
+sed -i "s/'\.\/renderer'/'.\/renderer.js'/" js/index.js
+sed -i "s/'\.\/input'/'.\/input.js'/" js/index.js
 echo "Done!"
